bbe66909d4f7c968eee3dc76d85c6afa197c857d
Qt <KylinMania@163.com>
2019-10-12 10:12:03 +0800

    maven struct

---
+++ src/test/java/net/sf/test/Test1.java
@@ -0,0 +1,17 @@
+package net.sf.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author Qt
+ * @since Oct 12, 2019
+ */
+@Slf4j
+public class Test1 {
+
+	@Test
+	public void test01() {
+		log.info("test");
+	}
+}
